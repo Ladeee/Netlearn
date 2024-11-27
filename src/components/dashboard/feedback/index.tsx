3827aedb052b0c3656dc9f238d133f68070b110a
OmoladeTrust <ladeakingboye@gmail.com>
2024-11-27 17:29:22 +0100

    my learning, course and insights

--- src/components/dashboard/feedback/index.tsx
+++ src/components/dashboard/feedback/index.tsx
@@ -1,5 +1,3 @@
-// import Align from "../../../assets/align.png"
-// import Audio from "../../../assets/voice.png"
 import Channel from "./channel"
 import "./feedback.scss"
 import Focus from "./focus"
